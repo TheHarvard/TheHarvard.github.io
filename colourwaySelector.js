293b192fd99bdc4caf2b88dc8ce6ce763e1ea178
Håvard Bakke <62014726+TheHarvard@users.noreply.github.com>
2024-05-23 15:44:44 +0200

    Colourway cookie test

---
+++ colourwaySelector.js
@@ -0,0 +1,41 @@
+// Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Function to get a cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Function to switch the stylesheet
+function setColourway(sheet) {
+    document.getElementById('colourwayCss').href = sheet;
+    setCookie('colourwayCss', sheet, 30); // Save the selected stylesheet for 30 days
+}
+
+// Function to apply the saved stylesheet on page load
+function setColourwayFromCookie() {
+    var cookieColourway = getCookie('colourwayCss');
+    if (cookieColourway) {
+        document.getElementById('colourwayCss').href = cookieColourway;
+    }
+}
+
+// Apply the saved stylesheet when the page loads
+window.onload = function() {
+    setColourwayFromCookie();
+}
