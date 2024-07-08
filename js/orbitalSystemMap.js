6dbc3675ff5bd6ff41124f2c3dbbccac94fea1a3
Håvard Bakke <62014726+TheHarvard@users.noreply.github.com>
2024-07-08 17:35:34 +0200

    correct (aprox) date label, and other labels

--- js/orbitalSystemMap.js
+++ js/orbitalSystemMap.js
@@ -57,6 +57,7 @@ function orbitalSystemMap_main(finalPass=false) {
         console.log("stage: ",stages[index]);
         
         renderOrbits(layer,staticOrbits[index]);
+        adTimeLabel(layer,0)
         layer.draw();
         element.style.display = ''; //remove style="display:none;"
         
@@ -72,7 +73,8 @@ function orbitalSystemMap_main(finalPass=false) {
             stage.destroyChildren();
             var layer = new Konva.Layer();
             stage.add(layer);
-            renderOrbits(layer,staticOrbits[index],frame.time/70);
+            renderOrbits(layer,staticOrbits[index],frame.time*8640);
+            adTimeLabel(layer,frame.time*8640)
             scaleStage();
             //layer.draw();
             });
@@ -169,6 +171,24 @@ function renderOrbits(layer,orbits, time = 0, offset = {"x":0,"y":0}){
 
         //console.log(key, orbits[key]);
 
+        // fill in missing data:
+        if (orbits[key].label===null || orbits[key].label === undefined) { orbits[key].label = ""; }
+        if (orbits[key].icon_r===null || orbits[key].icon_r === undefined) { orbits[key].icon_r = 0; }
+        if (orbits[key].icon_type===null || orbits[key].icon_type === undefined) { orbits[key].icon_type = ""; }
+        if (orbits[key].orbit_w===null || orbits[key].orbit_w === undefined) { orbits[key].orbit_w = 0; }
+        if (orbits[key].orbit_type===null || orbits[key].orbit_type === undefined) { orbits[key].orbit_type = ""; }
+        if (orbits[key].period===null || orbits[key].period === undefined) { orbits[key].period = 0; }
+        if (orbits[key].period_offset===null || orbits[key].period_offset === undefined) { orbits[key].period_offset = 0; }
+
+        if ((orbits[key].minor_axis===null || orbits[key].minor_axis === undefined) 
+            && (orbits[key].major_axis===null || orbits[key].major_axis === undefined))
+            { orbits[key].major_axis = 0; }
+
+        if (orbits[key].minor_axis===null || orbits[key].minor_axis === undefined) { orbits[key].minor_axis = orbits[key].major_axis; }
+        if (orbits[key].major_axis===null || orbits[key].major_axis === undefined) { orbits[key].major_axis = orbits[key].minor_axis; }
+        if (orbits[key].focal_axis_offset===null || orbits[key].focal_axis_offset === undefined) { orbits[key].focal_axis_offset = 0; }
+        if (orbits[key].satellites===null || orbits[key].satellites === undefined) { orbits[key].satellites = {}; }
+
         //calculate offsets
         let new_offset = getPositionFromOrbit(orbits[key],time);
         total_offset = {"x":0,"y":0};
@@ -178,11 +198,13 @@ function renderOrbits(layer,orbits, time = 0, offset = {"x":0,"y":0}){
         //console.log("new offset", new_offset);
         //console.log("total offset", total_offset);
 
-        //render the orbit
-        orbits[key].konva_orbit = getEllipseFromOrbit(orbits[key]);
-        orbits[key].konva_orbit.x(115+offset.x);
-        orbits[key].konva_orbit.y(0+offset.y);
-        layer.add(orbits[key].konva_orbit);
+        if (typeof orbits[key].orbit_w === 'number' && orbits[key].orbit_w > 0) {
+            //render the orbit
+            orbits[key].konva_orbit = getEllipseFromOrbit(orbits[key]);
+            orbits[key].konva_orbit.x(115+offset.x);
+            orbits[key].konva_orbit.y(0+offset.y);
+            layer.add(orbits[key].konva_orbit);
+    }
 
 
         if (typeof orbits[key].icon_r === 'number' && orbits[key].icon_r > 0) {
@@ -208,12 +230,19 @@ function renderOrbits(layer,orbits, time = 0, offset = {"x":0,"y":0}){
         }
     }
 
-    //reorder all elements in layer so text boxes are on top.
-    layer.getChildren().forEach((shape) => {
-        if (shape instanceof Konva.Text) {
-          shape.moveToTop();
-        }
-      });
+
+    ////reorder all elements in layer so text boxes are on top.
+    //layer.getChildren().forEach((shape) => {
+    //    if (shape instanceof Konva.Text) {
+    //      shape.moveToTop();
+    //    }
+    //  });
+
+    //cache and apply blur on entire layer
+    //layer.cache();
+    // Apply the blur filter to the layer
+    //layer.filters([Konva.Filters.Blur]);
+    //layer.blurRadius(0.001); // Set the blur radius
 
 } 
 
@@ -287,13 +316,13 @@ function scaleStage() {
 
 //creates Konva.Ellipse from orbit data
 function getEllipseFromOrbit(orbitParams) {
-    const { major_axis, minor_axis, focal_axis_offset } = orbitParams;
+    let { orbit_w, orbit_type, major_axis, minor_axis, focal_axis_offset } = orbitParams;
 
     // Calculate the distance to the focal point
     const c = Math.sqrt(Math.pow(major_axis, 2) - Math.pow(minor_axis, 2));
 
     // Create and return the Konva.Ellipse
-    return new Konva.Ellipse({
+    let ellipse = new Konva.Ellipse({
         preventDefault: false,
         x: 0,  // Initial position, can be updated later
         y: 0,  // Initial position, can be updated later
@@ -303,18 +332,26 @@ function getEllipseFromOrbit(orbitParams) {
         radiusY: minor_axis,
         stroke: "rgb(255, 176, 0)",
         shadowColor: "rgb(255, 176, 0)",
-        shadowBlur: 1,
+        shadowBlur: 0,
         shadowOffsetX: 0,
         shadowOffsetY: 0,
         //dashEnabled: true,
-        //dash: [10, 10],
-        strokeWidth: 0.4,
+        //dash: [1, 1],
+        strokeWidth: orbit_w,
         rotation: focal_axis_offset,  // Apply the rotation
         offset: {
             x: c,  // Offset to rotate around the right focal point
             y: 0
         }
     });
+
+
+    if (orbit_type==="dash") {
+        ellipse.dashEnabled(true);
+        ellipse.dash([5, 20]);
+    }
+
+    return ellipse;
 }
 
 //creates Konva.Ellipse from orbit data
@@ -339,7 +376,7 @@ function getIconFromOrbit(orbitParams) {
         stroke: "rgb(255, 176, 0)",
         fill: "rgb(255, 176, 0)",
         shadowColor: "rgb(255, 176, 0)",
-        shadowBlur: 1,
+        shadowBlur: 0,
         shadowOffsetX: 0,
         shadowOffsetY: 0,
     });
@@ -366,6 +403,9 @@ function getPositionFromOrbit(orbitParams, time) {
     time=0;
     period=360;
     };
+
+    //scale time to days
+    time = time / 864000;
 
     // Calculate the mean anomaly
     const meanAnomaly = (((-time) + period_offset) / period) * 2 * Math.PI;
@@ -435,6 +475,53 @@ function getLabelFromOrbit(orbitParams){
     }
 
     return textLabel;
+}
+
+
+//Add time label. 
+function adTimeLabel(layer, time) {
+    //unicode time
+    let unicodeTime = Date.now();
+
+    //offset
+    let currentTime  = (time*100) + 37868342400*1000; // 1200 year offset
+
+    // Example function to pad single digit numbers with leading zero
+    function pad2(number) {
+        return (number < 10 ? '0' : '') + number;
+    }
+    
+    // Format date into YYYYMMDD HH:MM:SS
+    let date = new Date(currentTime);
+    //let formattedDate = `${date.getUTCFullYear()}-${pad2(date.getUTCMonth() + 1)}-${pad2(date.getUTCDate())} ${pad2(date.getUTCHours())}:${pad2(date.getUTCMinutes())}:${pad2(date.getUTCSeconds())}`;
+    let formattedDate = `${date.getUTCFullYear()}-${pad2(date.getUTCMonth() + 1)}-${pad2(date.getUTCDate())}`;
+    
+    // Get day of the week name
+    let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let dayOfWeek = daysOfWeek[date.getUTCDay()];
+
+    // Concatenate day of the week to the formatted date
+    //let displayString = `${formattedDate} ${dayOfWeek}`;
+    let displayString = `${formattedDate}`;
+
+    let konva_date_label = new Konva.Text({
+        preventDefault: false,
+        x: 0,  
+        y: -100,  
+        text: displayString,
+        fontSize: 5,
+        fontFamily: "Noto Sans Mono",
+        align: "center",
+        strokeWidth: 0,
+        fill: "rgb(255, 176, 0)",
+        stroke: "rgb(255, 176, 0)",
+        shadowColor: "rgb(255, 176, 0)",
+        shadowBlur: 1,
+        shadowOffsetX: 0,
+        shadowOffsetY: 0,
+    });
+
+    layer.add(konva_date_label);
 }
 
 
